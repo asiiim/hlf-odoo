4271811b58361fbfefb0dfaac43d666e2208d664
manojlkdka <manojlkdka@gmail.com>
2022-05-15 14:21:45 +0545

    product qty update from package in line

---
+++ product_package_qty_update/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Package Qty",
+
+    'summary': """
+        Quantity change based on product package""",
+
+    'author': "Manoj Khadka, 10 Orbits",
+    'website': "https://www.10orbits.com/",
+    'category': 'Contact',
+    'version': '14.0.1.0.0',
+
+    'depends': ['sale','sale_stock'],
+
+    'data': [
+    ],
+}
